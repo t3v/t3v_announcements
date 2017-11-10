1a1ebf5287b146a50dcbbc6aeeec56bb02cea955
Maik Kempe <mkempe@bitaculous.com>
2017-11-10 17:34:17 +0100

    [➠] Refactored TypoScript.

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,4 +1,6 @@
-# === T3v Announcements Configuration ===
+# -----------------------------------
+# | T3v Announcements Configuration |
+# -----------------------------------
 
 t3v {
   announcements {
@@ -14,7 +16,62 @@ t3v {
   }
 }
 
+# === Plugin Configuration ===
+
+plugin {
+  tx_t3vannouncements {
+    persistence {
+      classes {
+        T3v\T3vAnnouncements\Domain\Model\JobAd {
+          mapping {
+            tableName = tx_t3vannouncements_domain_model_job_ad
+
+            # columns {
+            #   foo.mapOnProperty = foo
+            #   bar.mapOnProperty = bar
+            # }
+          }
+        }
+
+        T3v\T3vAnnouncements\Domain\Model\NewsArticle {
+          mapping {
+            tableName = tx_t3vannouncements_domain_model_news_article
+
+            # columns {
+            #   foo.mapOnProperty = foo
+            #   bar.mapOnProperty = bar
+            # }
+          }
+        }
+      }
+
+      enableAutomaticCacheClearing = {$plugin.tx_t3vannouncements.persistence.enableAutomaticCacheClearing}
+
+      storagePid = {$plugin.tx_t3vannouncements.persistence.storagePid}
+
+      updateReferenceIndex = {$plugin.tx_t3vannouncements.persistence.updateReferenceIndex}
+    }
+
+    settings {
+      # ...
+    }
+
+    view {
+      layoutRootPaths {
+        0 = {$plugin.tx_t3vannouncements.view.layoutRootPath}
+      }
+
+      templateRootPaths {
+        0 = {$plugin.tx_t3vannouncements.view.templateRootPath}
+      }
+
+      partialRootPaths {
+        0 = {$plugin.tx_t3vannouncements.view.partialRootPath}
+      }
+    }
+  }
+}
+
 # === Includes ===
 
-<INCLUDE_TYPOSCRIPT: source="FILE: ./Vendor/setup.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE: ./Library/setup.ts">
